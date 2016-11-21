a92c4fa966910fe5dede32e3adb6e910bdaa8492
Nick Oki <nick.oki@live.com>
2016-11-21 18:54:07 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+// index.js
 
+// ====================
+// Dependencies
+// ====================
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import './index.css'
+
+
+// ====================
+// Render
+// ====================
 ReactDOM.render(
   <App />,
   document.getElementById('root')
-);
+)
