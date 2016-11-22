957e620c361481cba6719eab57899c69352188f0
Nick Oki <nick.oki@live.com>
2016-11-21 20:59:39 -0500

    Update Home.js to test queryApi() from Utils.js

--- src/Home.js
+++ src/Home.js
@@ -4,6 +4,7 @@
 // Dependencies
 // ====================
 import React, { Component } from 'react'
+import {queryApi} from './Utils'
 import './Home.css';
 
 
@@ -12,10 +13,29 @@ import './Home.css';
 // Class Definition & Render
 // ====================
 class Home extends Component {
+  // Constructor
+  // constructor(props) {
+  //   super(props)
+  // }
+
+  logIn(e) {
+    let data = {
+      "email": "nick@nick.nick",
+      "password": "123123",
+    }
+    queryApi('/authenticate', 'POST', data).then( res => {
+      if (res.success === true) {
+        localStorage.setItem('remixologyUser', res.token)
+      }
+    })
+  }
+
+
   render() {
     return(
       <div className="Home">
         <h1>Hello, Nick.</h1>
+        <a onClick={e => this.logIn(e)}>Log In</a>
       </div>
     )
   }
--- src/Utils.js
+++ src/Utils.js
@@ -1,1 +1,22 @@
 // Utils.js
+
+// ====================
+// Dependencies
+// ====================
+import $ from 'jquery'
+// const API_URL = 'http://api.remixology.io'
+const API_URL = 'http://localhost:7000'
+
+
+// ====================
+// Exports
+// ====================
+export function queryApi(uri, type, body, headers) {
+  return $.ajax({
+    url: API_URL + uri,
+    // contentType: 'application/json',
+    method: type,
+    dataType: 'json',
+    data: body,
+  })
+}
